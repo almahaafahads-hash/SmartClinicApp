6f50a6c56674077464ea23b058018838c060b6ee
almahaafahads-hash <almahaafahads@gmail.com>
2026-02-14 16:26:56 +0300

    add

---
+++ SmartClinicApp/Domain/Doctors/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartClinicApp.Domain.Doctors
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+    }
+}
