efbe82dc4a4fc2845386b029b67e8c08bc17a848
Norah-Alqahtani2026 <noory.q22@gmail.com>
2026-02-14 18:02:11 +0300

    Created DoctorRepository implementation.

---
+++ SmartClinicApp/Repositories/DoctorRepository.cs
@@ -0,0 +1,17 @@
+﻿using SmartClinicApp.Domain.Doctors;
+using SmartClinicApp.Interfaces;
+
+namespace SmartClinicApp.Repositories
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        public IEnumerable<Doctor> GetAllDoctors()
+        {
+            return new List<Doctor>
+            {
+                new Doctor { Id = 1, Name = "د. خالد", Specialization = "باطنية" },
+                new Doctor { Id = 2, Name = "د. هند", Specialization = "أطفال" }
+            };
+        }
+    }
+}
