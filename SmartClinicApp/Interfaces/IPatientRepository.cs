0ce3a69f13c6107b4cdfea9bc02cb6a367992fd8
almahaafahads-hash <almahaafahads@gmail.com>
2026-02-14 17:29:50 +0300

    Interfaces folder and IPatientRepository

---
+++ SmartClinicApp/Interfaces/IPatientRepository.cs
@@ -0,0 +1,10 @@
+﻿using SmartClinicApp.Domain.Patients; // نربطه بمودل المريض اللي سويتيه
+
+namespace SmartClinicApp.Interfaces
+{
+    public interface IPatientRepository
+    {
+        // تعريف الدالة اللي بتجيب كل المرضى (بدون كود تنفيذي)
+        IEnumerable<Patient> GetAllPatients();
+    }
+}
