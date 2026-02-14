e64b70c5f2766666825b39642fa7bc0ed112920d
Norah-Alqahtani2026 <noory.q22@gmail.com>
2026-02-14 17:41:06 +0300

    Add IDoctorRepository.

---
+++ SmartClinicApp/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,10 @@
+﻿using SmartClinicApp.Domain.Doctors; // استدعاء مودل الدكتور
+
+namespace SmartClinicApp.Interfaces
+{
+    public interface IDoctorRepository
+    {
+        // هذه الدالة هي "العقد" اللي يضمن لنا استرجاع قائمة الأطباء
+        IEnumerable<Doctor> GetAllDoctors();
+    }
+}
