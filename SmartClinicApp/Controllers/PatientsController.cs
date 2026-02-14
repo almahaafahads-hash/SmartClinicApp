4d264f2a5dc635c3a4fa11950fded41b777ddf84
almahaafahads-hash <almahaafahads@gmail.com>
2026-02-14 18:10:22 +0300

    Updated PatientsController to use Repository.

--- SmartClinicApp/Controllers/PatientsController.cs
+++ SmartClinicApp/Controllers/PatientsController.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SmartClinicApp.Domain.Patients; // الربط بملف المريض
+using SmartClinicApp.Interfaces;
+using SmartClinicApp.Repositories;
 
 namespace SmartClinicApp.Controllers
 {
     public class PatientsController : Controller
     {
-        // عرض قائمة المرضى
+        private readonly IPatientRepository _patientRepo;
+
+        public PatientsController(IPatientRepository patientRepo)
+        {
+            _patientRepo = patientRepo;
+        }
+
         public IActionResult Index()
         {
-            var patients = new List<Patient>
-            {
-                new Patient { Id = 1, FullName = "ريما محمد", MedicalHistory = "متابعة" },
-                new Patient { Id = 2, FullName = "سارة المنصور", MedicalHistory = "فحص" }
-            };
-
+            // نطلب قائمة المرضى من الريبو
+            var patients = _patientRepo.GetAllPatients();
             return View(patients);
         }
     }
