67d7133799159cdb4970514afb3cabf11dc81c69
almahaafahads-hash <almahaafahads@gmail.com>
2026-02-14 16:51:38 +0300

    Added Patients Controller

---
+++ SmartClinicApp/Controllers/PatientsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartClinicApp.Domain.Patients; // استدعاء مودل المريض
+
+namespace SmartClinicApp.Controllers
+{
+    public class PatientsController : Controller
+    {
+        // الأكشن اللي تعرض قائمة المرضى
+        public IActionResult Index()
+        {
+            var patients = new List<Patient>
+            {
+                new Patient { Id = 1, FullName = "ريما محمد ", MedicalHistory = "فحص دوري" },
+                new Patient { Id = 2, FullName = "سارة المنصور", MedicalHistory = "متابعة سكري" }
+            };
+            return View(patients); // ترسل البيانات للـ View
+        }
+    }
+}
