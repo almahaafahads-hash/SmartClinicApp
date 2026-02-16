6e10759210d56f4f32cb36b3fb95ed72220ddbbc
Norah-Alqahtani2026 <noory.q22@gmail.com>
2026-02-16 20:18:33 +0300

    إعداد ApplicationDbContext وتعريف جداول الأطباء والمرضى والمواعيد تمهيداً للربط مع SQL Server"

---
+++ SmartClinicApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartClinicApp.Domain;
+
+namespace SmartClinicApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // جداول قاعدة البيانات
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
