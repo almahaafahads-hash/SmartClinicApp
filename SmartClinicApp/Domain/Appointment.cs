6acc5e6f634ead61fd37e3e330a6d311ef064863
almahaafahads-hash <almahaafahads@gmail.com>
2026-02-16 18:39:15 +0300

    تأسيس بنية نظام المواعيد (Domain & Interface) وتنظيف ملفات المشروع المكررة"

---
+++ SmartClinicApp/Domain/Appointment.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartClinicApp.Domain
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; } // رقم المريض المرتبط بالموعد
+        public int DoctorId { get; set; }  // رقم الدكتور المرتبط بالموعد
+        public DateTime AppointmentDate { get; set; } // تاريخ ووقت الموعد
+        public string Notes { get; set; } // ملاحظات (اختياري)
+    }
+}
---
+++ SmartClinicApp/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using SmartClinicApp.Domain;
+
+namespace SmartClinicApp.Interfaces
+{
+    public interface IAppointmentRepository
+    {
+        // أمر لجلب كل المواعيد
+        IEnumerable<Appointment> GetAllAppointments();
+
+        // أمر لإضافة موعد جديد
+        void AddAppointment(Appointment appointment);
+    }
+}
