1783c1de7cfc9448cb1e8656a37b01ddb0322b9d
Norah-Alqahtani2026 <noory.q22@gmail.com>
2026-02-17 20:41:02 +0300

    تعديل كود البروجرام

--- SmartClinicApp/Program.cs
+++ SmartClinicApp/Program.cs
@@ -1,50 +1,35 @@
-
 using Microsoft.EntityFrameworkCore;
 using SmartClinicApp.Data;
 using SmartClinicApp.Interfaces;
-﻿using SmartClinicApp.Interfaces;
 using SmartClinicApp.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IAppointmentRepository, AppointmentRepository>();
-// Add services to the container.
+
+// MVC
 builder.Services.AddControllersWithViews();
-//<<<<<<< HEAD
 
+// DbContext (SQL Server)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
-
-// الربط بين الانترفيس والتنفيذ (Dependency Injection)
-builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+// Dependency Injection (Repositories)
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
-builder.Services.AddSingleton<IAppointmentRepository, AppointmentRepository>();// هذا اللي يشغل نظام المواعيد ف الموقع
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));//تشغيل قاعددة في البرنامج
-
-// إضافة سطر تعريف قاعدة البيانات
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
