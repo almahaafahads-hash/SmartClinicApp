a95fef0d3bf0c68809fb94d824b36a596d1ae88f
almahaafahads-hash <almahaafahads@gmail.com>
2026-02-17 21:00:57 +0300

    EDIT

--- SmartClinicApp/Program.cs
+++ SmartClinicApp/Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 // Dependency Injection (Repositories)
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 builder.Services.AddSingleton<IAppointmentRepository, AppointmentRepository>();// هذا اللي يشغل نظام المواعيد ف الموقع
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -25,14 +25,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // إضافة سطر تعريف قاعدة البيانات//
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-=======
+
 builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
->>>>>>> 1783c1de7cfc9448cb1e8656a37b01ddb0322b9d
-=======
+
+
 builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
->>>>>>> 1783c1de7cfc9448cb1e8656a37b01ddb0322b9d
+
 
 var app = builder.Build();
 
